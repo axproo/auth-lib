dcad74d6663ea3dcd3af0c6ed2743b0765cc0363
Christian DJOMOU <developper@axproo.com>
2025-11-15 09:01:11 +0100

    Mise à jour

--- src/Libraries/TenantManager.php
+++ src/Libraries/TenantManager.php
@@ -2,22 +2,22 @@
 
 namespace Axproo\Auth\Libraries;
 
-use Axproo\Auth\Models\TenantModel;
+use Axproo\Auth\Models\UserTenantModel;
 
 class TenantManager
 {
-    protected TenantModel $model;
+    protected UserTenantModel $model;
 
     public function __construct() {
-        $this->model = new TenantModel();
+        $this->model = new UserTenantModel();
     }
 
     public function getTenantById(?int $userId) : ?string {
         if (!$userId) return null;
 
         $userTenant = $this->model
-            ->select('tenants.uuid')
-            ->join('tenants', 'tenants.id = users_tenants.tenant_id', 'left')
+            ->select(select: 't.uuid')
+            ->join('tenants AS t', 't.id = users_tenants.tenant_id', 'left')
             ->where('user_id', $userId)
             ->first();
         return $userTenant->uuid ?? null;
---
+++ src/Models/UserTenantModel.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace Axproo\Auth\Models;
+
+use CodeIgniter\Model;
+
+class UserTenantModel extends Model
+{
+    protected $table    = 'users_tenants';
+    protected $primaryKey = 'id';
+    protected $allowedFields = ['user_id','tenant_id','role','status'];
+    protected $returnType = 'Axproo\Auth\Entities\UserTenantEntity';
+    protected $useTimestamps = true;
+}
