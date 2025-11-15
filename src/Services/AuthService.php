0bb5e9a391c430daf2b062fdd445960521c3d6c4
Christian DJOMOU <developper@axproo.com>
2025-11-15 07:39:23 +0100

    Mise à jour

--- src/Services/AuthService.php
+++ src/Services/AuthService.php
@@ -2,16 +2,24 @@
 
 namespace Axproo\Auth\Services;
 
+use Axproo\Auth\Config\Validation\AuthConfig;
+
 class AuthService extends BaseAuthService
 {
+    protected $valid;
+
     public function __construct() {
         parent::__construct();
+        $this->valid = new AuthConfig;
     }
 
     public function login() {
         $data = $this->get_data_from_post();
 
-        // Validation du mot de passe
+        // Validation des données
+        if (!$this->validate($this->valid->auth)) {
+            return $this->respondError($this->validation->getErrors());
+        }
         return $this->respondSuccess('Success connexion', [
             'data' => $data
         ]);
