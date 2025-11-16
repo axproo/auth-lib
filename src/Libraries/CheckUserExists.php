7b503214ba553882c094bd0c52413323e63e4dfa
Christian DJOMOU <developper@axproo.com>
2025-11-16 02:38:20 +0100

    Mise à jour

--- src/Libraries/CheckUserExists.php
+++ src/Libraries/CheckUserExists.php
@@ -2,14 +2,25 @@
 
 namespace Axproo\Auth\Libraries;
 
+use Axproo\Auth\Exceptions\AuthException;
+use Axproo\Auth\Models\UsersModel;
+
 class CheckUserExists
 {
+    protected UsersModel $model;
 
     public function __construct() {
-        //
+        $this->model = new UsersModel();
     }
 
     public function handle(array $data) : array {
-        return ['check user' => $data];
+        $email = $data['email'] ?? null;
+
+        if (!$email) {
+            throw new AuthException(lang('Email.required'), 400);
+        }
+
+        $user = $this->model->where('email', $email)->first();
+        return ['user' => $user];
     }
 }
