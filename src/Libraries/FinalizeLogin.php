e359ac92c20e860b6359462bc695636f151759bd
Christian DJOMOU <developper@axproo.com>
2025-11-16 22:06:31 +0100

    Update auth-lib

--- src/Libraries/FinalizeLogin.php
+++ src/Libraries/FinalizeLogin.php
@@ -5,17 +5,18 @@ namespace Axproo\Auth\Libraries;
 use Axproo\Auth\Exceptions\AuthException;
 use Axproo\Auth\Models\UsersModel;
 use Axproo\Otp\Libraries\TokenManager;
+use CodeIgniter\I18n\Time;
 
 class FinalizeLogin
 {
     protected TokenManager $token;
-    // protected UsersModel $model;
+    protected UsersModel $model;
     protected TenantManager $tenant;
     protected RoleManager $rules;
 
     public function __construct() {
         $this->token = new TokenManager();
-        // $this->model = new UsersModel();
+        $this->model = new UsersModel();
         $this->tenant = new TenantManager();
         $this->rules = new RoleManager();
     }
@@ -24,22 +25,16 @@ class FinalizeLogin
         $user = $data['user'] ?? null;
         if (!$user) throw new AuthException(lang('Users.missing'), 500);
 
-        // $token = $this->token->generateToken([
-        //     // 'tenant' => $this->tenant->getTenantById($user['id']),
-        //     'email' => $user['email'],
-        //     // 'role' => $this->rules->getRoleById($user['id']),
-        //     // 'redirect' => ''
-        // ]);
-
-
-        // // $token = $this->token->generateToken([
-        // //     'tenant' => $this->tenant->getTenantById($user->id),
-        // //     'email' => $user->email,
-        // //     'role' => $this->rules->getRoleById($user->role_id),
-        // //     'redirect' => $this->redirectTo($user)
-        // // ]);
-        return [
-            'email' => $user->email
-        ];
+        $token = $this->token->generateToken([
+            'tenant' => $this->tenant->getTenantById($user->id),
+            'email' => $user->email,
+            'role' => $this->rules->getRoleById($user->id)
+        ]);
+        
+        // update last connexion
+        $user->last_login_at = Time::now();
+        $user->ip_address = $data['ip_address'] ?? null;
+        // $this->model->save($user);
+        return $data;
     }
 }
