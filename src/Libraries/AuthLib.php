2eb40dd0f81c4cb99041debe56f4731200ee9a4a
Christian DJOMOU <developper@axproo.com>
2025-11-16 02:06:31 +0100

    Mise à jour

--- src/Libraries/AuthLib.php
+++ src/Libraries/AuthLib.php
@@ -2,14 +2,22 @@
 
 namespace Axproo\Auth\Libraries;
 
+use Axproo\Auth\Exceptions\AuthException;
+
 class AuthLib
 {
-    protected array $steps = [];
+    protected array $steps = [
+        CheckUserExists::class
+    ];
 
     public function handle(array $payload) : array {
         $data = $payload;
 
-
+        foreach ($this->steps as $stepClass) {
+            if (!class_exists($stepClass)) {
+                throw new AuthException("Step class {$stepClass} not found", 500);
+            }
+        }
         return $data;
     }
 }
