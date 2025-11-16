8e70a30d0a0fcec79f131ef49b18e23f204395fd
Christian DJOMOU <developper@axproo.com>
2025-11-16 01:58:16 +0100

    Mise à jour

--- src/Services/BaseAuthService.php
+++ src/Services/BaseAuthService.php
@@ -11,20 +11,20 @@ use Config\Services;
 
 abstract class BaseAuthService
 {
-    protected $model;
+    // protected $model;
     protected $request;
     protected $validation;
-    protected TokenManager $token;
-    protected PasswordManager $hasher;
-    protected TenantManager $tenant;
-    protected RoleManager $rules;
+    // protected TokenManager $token;
+    // protected PasswordManager $hasher;
+    // protected TenantManager $tenant;
+    // protected RoleManager $rules;
 
     public function __construct() {
-        $this->model = new UsersModel();
-        $this->hasher = new PasswordManager();
-        $this->token = new TokenManager();
-        $this->tenant = new TenantManager();
-        $this->rules = new RoleManager();
+        // $this->model = new UsersModel();
+        // $this->hasher = new PasswordManager();
+        // $this->token = new TokenManager();
+        // $this->tenant = new TenantManager();
+        // $this->rules = new RoleManager();
         $this->request = service('request');
         $this->validation = Services::validation();
     }
@@ -44,15 +44,15 @@ abstract class BaseAuthService
         return axprooResponse($code, $message, $data);
     }
 
-    protected function checkStatus($user) {
-        switch ($user->status) {
-            case 'active': return true;
-            case 'pending': return '/verify-email';
-            case 'blocked': throw new \Exception("Account.blocked");
-            case 'inactive': throw new \Exception(lang('Account.inactive'));
-            default: throw new \Exception(lang('Status.unknown', ['status' => $user->status]));
-        }
-    }
+    // protected function checkStatus($user) {
+    //     switch ($user->status) {
+    //         case 'active': return true;
+    //         case 'pending': return '/verify-email';
+    //         case 'blocked': throw new \Exception("Account.blocked");
+    //         case 'inactive': throw new \Exception(lang('Account.inactive'));
+    //         default: throw new \Exception(lang('Status.unknown', ['status' => $user->status]));
+    //     }
+    // }
 
     protected function get_data_from_post() {
         return (array) $this->request->getVar();
