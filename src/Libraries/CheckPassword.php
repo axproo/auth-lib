d4f466b680a58d738a17c58473c2716e632f8199
Christian DJOMOU <developper@axproo.com>
2025-11-16 08:08:01 +0100

    Update auth-lib

--- src/Libraries/CheckPassword.php
+++ src/Libraries/CheckPassword.php
@@ -16,7 +16,7 @@ class CheckPassword
 
         // Password verification
         if (!password_verify($password, $user->password)) {
-            throw new AuthException(lang('Auth.invalid_credential'), 402);
+            throw new AuthException(lang('Auth.invalid_credential'), 401);
         }
 
         $data['password_checked'] = true;
