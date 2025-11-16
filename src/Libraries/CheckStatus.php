94c6a634cd7ba0b0df9205812526363bf19e11af
Christian DJOMOU <developper@axproo.com>
2025-11-16 02:48:46 +0100

    Mise à jour

--- src/Libraries/CheckStatus.php
+++ src/Libraries/CheckStatus.php
@@ -2,9 +2,30 @@
 
 namespace Axproo\Auth\Libraries;
 
+use Axproo\Auth\Exceptions\AuthException;
+
 class CheckStatus
 {
-    // public function handle() : Returntype {
-        
-    // }
+    public function handle(array $payload) : array {
+        $user = $data['user'] ?? null;
+        if (!$user) {
+            throw new AuthException(lang('Users.missing'), 500);
+        }
+
+        $status = $user->status ?? 'active';
+
+        switch ($status) {
+            case 'pending':
+                throw new AuthException(lang('Account.pending'), 403, [
+                    'action' => 'verify_email'
+                ]);
+
+            case 'inactive': throw new AuthException(lang('Account.inactive'), 403);
+            case 'blocked': throw new AuthException(lang('Account.blocked'), 403);
+            case 'active':
+            default:
+                $data['user_status_checked'] = true;
+                return $data;
+        }
+    }
 }
