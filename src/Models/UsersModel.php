85fc40370ef8ac8acdb332c97ef9bf57c960fb41
Christian DJOMOU <developper@axproo.com>
2025-11-16 22:21:17 +0100

    Update auth-lib

--- src/Models/UsersModel.php
+++ src/Models/UsersModel.php
@@ -9,7 +9,7 @@ class UsersModel extends Model
     protected $table = 'users';
     protected $primaryKey = 'id';
     protected $useAutoIncrement = true;
-    protected $allowedFields = ['email','password','name','role_id','status','email_verified','email_verified_at'];
+    protected $allowedFields = ['email','password','name','role_id','status','email_verified','email_verified_at','two_factor_enabled','last_login_at','ip_address'];
     protected $returnType = 'Axproo\Auth\Entities\UsersEntity';
     protected $useTimestamps = true;
 
