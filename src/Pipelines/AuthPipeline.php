d40d42f3c3b05f0ba37f98a0b51c57ce402a5ad5
Christian DJOMOU <developper@axproo.com>
2025-11-24 23:24:42 +0100

    Update auth-lib dev

--- src/Pipelines/AuthPipeline.php
+++ src/Pipelines/AuthPipeline.php
@@ -4,6 +4,7 @@ namespace Axproo\Auth\Pipelines;
 
 use Axproo\Auth\Exceptions\AuthException;
 use Axproo\Auth\Steps\CheckUserExists;
+use ReflectionClass;
 
 class AuthPipeline
 {
@@ -23,9 +24,36 @@ class AuthPipeline
             if (!class_exists($stepClass)) {
                 throw new AuthException(lang('Steps.not_found', ['step' => $stepClass]), 500);
             }
-        }
 
-        $data['current_step'] = $currentStep;
+            $ref = new ReflectionClass($stepClass);
+            $step = $ref->newInstance();
+
+            if (!method_exists($step, 'handle')) {
+                throw new AuthException(lang('Steps.not_implement', [
+                    'step' => $stepClass,
+                    'method' => 'handle()'
+                ]), 500);
+            }
+
+            // Exécution de l'étape
+            $data = $step->handle($data);
+
+            if (!\is_array($data)) {
+                throw new AuthException(lang('Steps.not_array', [
+                    'step' => $stepClass
+                ]), 500);
+            }
+
+            // Si la step n'est pas valide -> arrêt immédiat
+            if (!empty($data['stop_here']) && $data['stop_here'] === true) {
+                session()->set('current_step', $i);
+                return $data;
+            }
+        }
+        
+        // Si toutes les étapes sont finalisées -> on supprime la progression
+        session()->remove('current_step');
+        $data['current_step'] = session()->get('current_step');
         return $data;
     }
 }
