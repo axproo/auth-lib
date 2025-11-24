6b08b42159ac66afd20493f2dfc14508333000be
Christian DJOMOU <developper@axproo.com>
2025-11-24 23:58:08 +0100

    Update auth-lib dev

--- src/Pipelines/AuthPipeline.php
+++ src/Pipelines/AuthPipeline.php
@@ -7,57 +7,18 @@ use Axproo\Auth\Steps\CheckStatus;
 use Axproo\Auth\Steps\CheckUserExists;
 use ReflectionClass;
 
-class AuthPipeline
+class AuthPipeline extends BasePipeline
 {
     protected array $step = [
         CheckUserExists::class,
         CheckStatus::class
     ];
 
+    public function __construct() {
+        parent::__construct($this->step);
+    }
+
     public function handle(array $payload) : array {
-        $data = $payload;
-
-        // Récupération de la progression
-        $currentStep = session()->get('current_step') ?? 0;
-
-        for ($i= $currentStep; $i < \count($this->step); $i++) { 
-            $stepClass = $this->step[$i];
-
-            if (!class_exists($stepClass)) {
-                throw new AuthException(lang('Steps.not_found', ['step' => $stepClass]), 500, [
-                    'stop_here' => true
-                ]);
-            }
-
-            $ref = new ReflectionClass($stepClass);
-            $step = $ref->newInstance();
-
-            if (!method_exists($step, 'handle')) {
-                throw new AuthException(lang('Steps.not_implement', [
-                    'step' => $stepClass,
-                    'method' => 'handle()'
-                ]), 500, ['stop_here' => true]);
-            }
-
-            // Exécution de l'étape
-            $data = $step->handle($data);
-
-            if (!\is_array($data)) {
-                throw new AuthException(lang('Steps.not_array', [
-                    'step' => $stepClass
-                ]), 500, ['stop_here' => true]);
-            }
-
-            // Si la step n'est pas valide -> arrêt immédiat
-            if (!empty($data['stop_here']) && $data['stop_here'] === true) {
-                session()->set('current_step', $i);
-                return $data;
-            }
-        }
-        
-        // Si toutes les étapes sont finalisées -> on supprime la progression
-        session()->remove('current_step');
-        $data['current_step'] = session()->get('current_step');
-        return $data;
+        return $this->handle($payload);
     }
 }
---
+++ src/Pipelines/BasePipeline.php
@@ -0,0 +1,62 @@
+<?php 
+
+namespace Axproo\Auth\Pipelines;
+
+use Axproo\Auth\Exceptions\AuthException;
+use ReflectionClass;
+
+abstract class BasePipeline
+{
+    protected array $step = [];
+
+    public function __construct(array $step = []) {
+        $this->step = $step;
+    }
+    
+    protected function handle(array $payload) : array {
+        $data = $payload;
+
+        // Récupération de la progression
+        $currentStep = session()->get('current_step') ?? 0;
+
+        for ($i= $currentStep; $i < \count($this->step); $i++) { 
+            $stepClass = $this->step[$i];
+
+            if (!class_exists($stepClass)) {
+                throw new AuthException(lang('Steps.not_found', ['step' => $stepClass]), 500, [
+                    'stop_here' => true
+                ]);
+            }
+
+            $ref = new ReflectionClass($stepClass);
+            $step = $ref->newInstance();
+
+            if (!method_exists($step, 'handle')) {
+                throw new AuthException(lang('Steps.not_implement', [
+                    'step' => $stepClass,
+                    'method' => 'handle()'
+                ]), 500, ['stop_here' => true]);
+            }
+
+            // Exécution de l'étape
+            $data = $step->handle($data);
+
+            if (!\is_array($data)) {
+                throw new AuthException(lang('Steps.not_array', [
+                    'step' => $stepClass
+                ]), 500, ['stop_here' => true]);
+            }
+
+            // Si la step n'est pas valide -> arrêt immédiat
+            if (!empty($data['stop_here']) && $data['stop_here'] === true) {
+                session()->set('current_step', $i);
+                return $data;
+            }
+        }
+        
+        // Si toutes les étapes sont finalisées -> on supprime la progression
+        session()->remove('current_step');
+        $data['current_step'] = session()->get('current_step');
+        return $data;
+    }
+}
